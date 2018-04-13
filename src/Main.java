f2a6359d646996b0fc527e1e514393ef56cddcb2
bruno <bruno@bruno-Aspire-V3-571>
2018-04-13 16:25:33 -0300

    COmmit teste

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,6 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Olá Mundo"); //ssss
-        System.out.println("Olá Mundo"); //ssss
     }
     
 }
