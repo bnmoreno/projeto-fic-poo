703983af2e1ef291716dfe37155d7f9fd11584c7
bruno <bruno@bruno-Aspire-V3-571>
2018-04-16 11:30:55 -0300

    COmmit teste 2

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Olá Mundo"); //ssss
-        System.out.println("Olá Mundo"); //ssss
     }
     
 }
